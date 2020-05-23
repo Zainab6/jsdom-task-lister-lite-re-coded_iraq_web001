e33e56fcdb5f93bf273070cee02371a22a698881
Zainab Saud <zai99nb@gmail.com>
2020-05-23 08:54:58 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.getElementById("submit").addEventListener("click",function() {
+    event.preventDefault();
+  })
+function addToDo() {
+  const list= document.createElement("li");
+  const input = document.getElementById("new-task-description").value;
+  
+  const uList = document.getElementById("tasks");
+  list.innerHTML = `${input}`;
+  uList.appendChild("list");
+  
+}
+  });
